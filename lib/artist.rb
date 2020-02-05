98807c77f0091010352b2bb19788e33e5d32d9af
Steven Perfors <stevecanbuild@gmail.com>
2020-02-05 17:16:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,8 @@
 class Artist 
-  attr_accessor :name, :songs  
+  attr_accessor :name  
   
   def initialize(name)
     @name = name
-    @songs = []
-  end
-  
-  def add_song(song)
-    @songs << song unless @songs.include?(song)
-    song.artist = self if song.artist == nil 
   end
  
 end
